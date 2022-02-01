c168bd46ee41b6e0916b822eed0dacfd521e9ba7
Dennis Chavours <dchavours@gmail.com>
2022-02-01 04:08:16 -0600

    Added course field

--- WooOMA/partials/course-manager-admin-display.php
+++ WooOMA/partials/course-manager-admin-display.php
@@ -38,10 +38,9 @@
 <form action="/action_page.php">
   <label for="cars">Enter Course Name:</label>
   <select name="cars" id="cars">
-    <option value="volvo">CPR</option>
-    <option value="saab">Saab</option>
-    <option value="opel">Opel</option>
-    <option value="audi">Audi</option>
+  <?php foreach ( WC_Bookings_Admin::get_booking_products() as $product ) : ?>
+								<option value="<?php echo esc_attr( $product->get_id() ); ?>"><?php echo esc_html( sprintf( '%s (#%s)', $product->get_name(), $product->get_id() ) ); ?></option>
+		<?php endforeach; ?>
   </select>
   <br><br>
 </form>
@@ -55,6 +54,11 @@
     <option value="saab">Saab</option>
     <option value="opel">Opel</option>
     <option value="audi">Audi</option>
+    <?php foreach ( WC_Bookings_Admin::get_booking_products() as $product ) : ?>
+								<option value="<?php echo esc_attr( $product->get_id() ); ?>"><?php echo esc_html( sprintf( '%s (#%s)', $product->get_name(), $product->get_id() ) ); ?></option>
+		<?php endforeach; ?>
+
+
   </select>
   <br><br>
 </form>
@@ -64,4 +68,5 @@
 	Then after that it will show a table of all the people and if they paid or not and make it so people haven't
 	paid are at the top of the list then after that, people who haven't paid for textbooks are below them. 
 	
-	
+	<?
+
