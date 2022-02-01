e465ad487ee6c6b5e44202d9482c5d0e34d1be72
Dennis Chavours <dchavours@gmail.com>
2022-02-01 00:55:55 -0600

    Added partials folder

---
+++ WooOMA/partials/course-manager-admin-display.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://wp2mag.blogspot.com
+ * @since      1.0.0
+ *
+ * @package    Course_Manager
+ * @subpackage Course_Manager/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
+
+      <!-- HTML --> 
+      <p>Enter Date: <input type = "text" id = "datepicker-13"></p>
+	
+	Then once they select the Date, then the next input comes up.
+
+	<h1>&nbsp;</h1>
+		
+
+
+<form action="/action_page.php">
+  <label for="cars">Enter Course Name:</label>
+  <select name="cars" id="cars">
+    <option value="volvo">CPR</option>
+    <option value="saab">Saab</option>
+    <option value="opel">Opel</option>
+    <option value="audi">Audi</option>
+  </select>
+  <br><br>
+</form>
+
+
+
+<form action="/action_page.php">
+  <label for="cars">Enter Time:</label>
+  <select name="cars" id="cars">
+    <option value="volvo">11:00 am</option>
+    <option value="saab">Saab</option>
+    <option value="opel">Opel</option>
+    <option value="audi">Audi</option>
+  </select>
+  <br><br>
+</form>
+
+
+	
+	Then after that it will show a table of all the people and if they paid or not and make it so people haven't
+	paid are at the top of the list then after that, people who haven't paid for textbooks are below them. 
+	
+	
